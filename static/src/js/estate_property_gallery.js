91ad9ab099e2af8b3a50f7be0a7e27516412f13a
Hexclad-Security <hexcladsec@gmail.com>
2026-02-01 19:03:07 -0500

    Fix gallery modal and lot stat icon

--- static/src/js/estate_property_gallery.js
+++ static/src/js/estate_property_gallery.js
@@ -2,10 +2,10 @@
 
 import publicWidget from "@web/legacy/js/public/public_widget";
 
-publicWidget.registry.HexEstateGallery = publicWidget.Widget.extend({
+publicWidget.registry.HexPropertyGallery = publicWidget.Widget.extend({
     selector: ".hex-estate-property-page",
     events: {
-        "click .estate-gallery [data-hex-gallery-index]": "_onGalleryClick",
+        "click .hex-property-thumb": "_onGalleryClick",
     },
 
     start() {
@@ -15,19 +15,25 @@ publicWidget.registry.HexEstateGallery = publicWidget.Widget.extend({
     },
 
     _onGalleryClick(event) {
-        const index = Number(event.currentTarget.dataset.hexGalleryIndex || 0);
-        if (!this.$carousel.length || Number.isNaN(index)) {
+        event.preventDefault();
+        const index = Number(event.currentTarget.dataset.index || 0);
+        if (!this.$carousel.length || Number.isNaN(index) || !window.bootstrap) {
             return;
         }
 
+        const modalElement = this.$modal.get(0);
         const carouselElement = this.$carousel.get(0);
-        if (!carouselElement || !window.bootstrap) {
+        if (!carouselElement || !modalElement) {
             return;
         }
 
+        const modal = window.bootstrap.Modal.getOrCreateInstance(modalElement);
         const carousel = window.bootstrap.Carousel.getOrCreateInstance(carouselElement, {
+            interval: false,
             ride: false,
+            wrap: true,
         });
         carousel.to(index);
+        modal.show();
     },
 });
